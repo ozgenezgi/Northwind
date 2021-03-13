9462872e13b4ee859e833fbeb0dc91dbfc8fbd6c
ezgi özgen <ozgenn.ezgi@gmail.com>
2021-03-13 23:54:37 +0300

    13 mart

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
 
 
@@ -16,10 +17,16 @@ export class ProductComponent implements OnInit {
   products:Product[] = [];
   dataLoaded = false;
   
-  constructor(private productService:ProductService) { }
+  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.getProducts();
+      this.activatedRoute.params.subscribe(params=>{
+        if(params["categoryId"]){
+          this.getProductsByCategory(params["categoryId"])
+        }else{
+          this.getProducts()
+        }
+      })
     
   }
   getProducts(){
@@ -30,6 +37,15 @@ export class ProductComponent implements OnInit {
     
 
   }
+
+  getProductsByCategory(categoryId:number){
+    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
+       this.products = response.data
+       this.dataLoaded =true;
+     })
+     
+ 
+   }
 
 
 
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,11 +7,18 @@ import { Product } from '../models/product';
   providedIn: 'root'
 })
 export class ProductService {
-  apiUrl = "https://localhost:44381/api/products/getall"
+  apiUrl = 'https://localhost:44381/api/';
   constructor(private httpClient:HttpClient) { }
   getProducts():Observable<ListResponseModel<Product>>{
+    let newPath = this.apiUrl + "products/getall"
     return this.httpClient
-    .get<ListResponseModel<Product>>(this.apiUrl);
+    .get<ListResponseModel<Product>>(newPath);
     
     }
+    getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>>{
+      let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId
+      return this.httpClient
+      .get<ListResponseModel<Product>>(newPath);
+      
+      }
 }
