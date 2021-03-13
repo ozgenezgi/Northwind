3ce60d5d3fba4541efa0c7e71ca7ec6f48c1ead7
ezgi özgen <ozgenn.ezgi@gmail.com>
2021-03-13 22:40:40 +0300

    13 Mart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'northwind';
+  user: string = "Ezgi Özgen"
+  product = { productId: 1, productName: 'Bardak', categoryId: 2, unitPrice: 5 }
+  product2 = { productId: 2, productName: 'laptop', categoryId: 1, unitPrice: 1115 }
+  product3 = { productId: 3, productName: 'Mouse', categoryId: 1, unitPrice: 65 }
+  product4 = { productId: 4, productName: 'Keyboard', categoryId: 1, unitPrice: 55 }
+
+
+  products = [this.product, this.product2, this.product3, this.product4]
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import{HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  apiUrl = "https://localhost:44381/api/products/getall"
+  constructor(private httpClient:HttpClient) { }
+  getProducts():Observable<ListResponseModel<Product>>{
+    return this.httpClient
+    .get<ListResponseModel<Product>>(this.apiUrl);
+    
+    }
+}
