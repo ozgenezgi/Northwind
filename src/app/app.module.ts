16f509e17470dc8be2ac1d158db08da5d2f8a5eb
ezgi özgen <ozgenn.ezgi@gmail.com>
2021-03-25 00:03:53 +0300

    25 mart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import{HttpClientModule} from '@angular/common/http';
+import{HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import{FormsModule, ReactiveFormsModule} from '@angular/forms';
 import{BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -8,13 +8,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
+import { TodoComponent } from './components/todo/todo.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 import {ToastrModule} from 'ngx-toastr';
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
-import { ProductAddComponent } from './components/product-add/product-add.component'
+import { ProductAddComponent } from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+
 
 
 
@@ -23,11 +27,15 @@ import { ProductAddComponent } from './components/product-add/product-add.compon
     AppComponent,
     ProductComponent,
     CategoryComponent,
+    TodoComponent,
     NaviComponent,
     VatAddedPipe,
     FilterPipePipe,
     CartSummaryComponent,
-    ProductAddComponent
+    ProductAddComponent,
+    LoginComponent,
+  
+    
     
    
   ],
@@ -42,7 +50,9 @@ import { ProductAddComponent } from './components/product-add/product-add.compon
       positionClass:"toast-bottom-right"
     })
   ],
-  providers: [],
+  providers: [{
+    provide:HTTP_INTERCEPTORS, useClass:AuthInterceptor, multi:true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
